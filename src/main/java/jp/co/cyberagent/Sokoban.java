fe627a4da8e8c2a095ee8b700ee3b9546f5fb706
Yuichiro Mukai <y.iky917@gmail.com>
2015-04-22 08:31:22 +0900

    initial commit

---
+++ src/main/java/jp/co/cyberagent/Sokoban.java
@@ -0,0 +1,11 @@
+package jp.co.cyberagent;
+
+import com.googlecode.lanterna.*;
+
+public class Sokoban {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello world");
+	}
+
+}
