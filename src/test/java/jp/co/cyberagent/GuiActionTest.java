15bbb5f280baecead4a0a2072194376862f3be6f
Yuichiro Mukai <y.iky917@gmail.com>
2015-04-22 14:04:41 +0900

    fix tests

--- src/test/java/jp/co/cyberagent/GuiActionTest.java
+++ src/test/java/jp/co/cyberagent/GuiActionTest.java
@@ -8,11 +8,11 @@ public class GuiActionTest {
 
     @Test
     public void testConvertFromChar() throws Exception {
-        assertEquals(GuiAction.MOVE_UP,    'k');
-        assertEquals(GuiAction.MOVE_RIGHT, 'l');
-        assertEquals(GuiAction.MOVE_DOWN,  'j');
-        assertEquals(GuiAction.MOVE_LEFT,  'h');
+        assertEquals(GuiAction.MOVE_UP,    GuiAction.convertFromChar('k'));
+        assertEquals(GuiAction.MOVE_RIGHT, GuiAction.convertFromChar('l'));
+        assertEquals(GuiAction.MOVE_DOWN,  GuiAction.convertFromChar('j'));
+        assertEquals(GuiAction.MOVE_LEFT,  GuiAction.convertFromChar('h'));
 
-        assertEquals(GuiAction.QUIT, 'q');
+        assertEquals(GuiAction.QUIT, GuiAction.convertFromChar('q'));
     }
 }
